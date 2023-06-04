e14daddfb4938273fcfe348604f8464e12875b85
Mario Osmakcic <osmakmar@students.zhaw.ch>
2023-06-04 17:32:34 +0200

    redeo change

--- frontend/src/app/gui-model/guimodel.ts
+++ frontend/src/app/gui-model/guimodel.ts
@@ -150,15 +150,6 @@ export class GuiModel {
                             "width": 2,
                             "newRow": false,
                         },
-                        {
-                            "type": "button",
-                            "name": { default: "Ebenfalls Button für ToDo-List" },
-                            "icon": "fa-file-alt",
-                            "color": "wet-asphalt",
-                            "page": "toDoPage",
-                            "width": 2,
-                            "newRow": false,
-                        },
                     ]
                 },
                 {
